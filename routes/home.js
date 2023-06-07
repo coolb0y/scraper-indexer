84a8c90116c7222dd2620e87d4f186acd0321f35
Ankur Narayan Singh <57170752+coolb0y@users.noreply.github.com>
2023-06-07 16:47:55 +0530

    Add files via upload

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
+const path = require('path');
 
 router.get('/', (req, res) => {
+    path.join(__dirname, '../views/index.html');
     return res.status(200).sendFile('index.html', { root: __dirname + "/../views/" });
 })
 
