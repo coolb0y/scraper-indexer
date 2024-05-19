07f9f8fc24397e18a84f831dee6ffec697b30627
coolb0y <57170752+coolb0y@users.noreply.github.com>
2024-05-19 18:48:02 +0530

    everything working fine

--- sample.js
+++ sample.js
@@ -1,100 +1,28 @@
-const ExifReader = require("exifreader");
-const fs = require("fs-extra");
-var gracefulFs = require("graceful-fs");
-gracefulFs.gracefulify(fs);
-const filePath = "C:/Users/gameS/OneDrive/Desktop/Chipsterankur4/Chipster4Imagetest/swimming boy.webp";
-const filePath2 = "C:/Users/gameS/OneDrive/Desktop/Chipsterankur4/Chipster4Imagetest/typing.gif";
-const filePath3 = "C:/Users/gameS/OneDrive/Desktop/Chipsterankur4/Chipster4Imagetest/fox.avif";
-const exiftool = require("exiftool-vendored").exiftool;
-// this is written to test some new formats we are considering
-const vendorFunc = ()=>{
-    exiftool
-  .read(filePath2)
-  .then((tags /*: Tags */) =>
-    console.log(
-      `Make: ${tags.Make}, Model: ${tags.Model}, Errors: ${tags.errors}`
-    )
-  )
-  .catch((err) => console.error("Something terrible happened: ", err))
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+
+
+
+// Function to maintain aspect ratio
+function maintainAspectRatio(height, aspectRatio = 16 / 9) {
+  const width = Math.round(height * aspectRatio);
+  return `${width}:${height}`;
 }
 
-const SomeFunc = async ()=>{
-
-    const imageBuffer = fs.readFileSync(filePath2);
-    const result = ExifReader.load(imageBuffer);
-    console.log(result);
-    
-    // let imgtitle = "";
-    // let imgtags = "";
-    // let imageWidth = result
-    //   ? result["Image Width"]
-    //     ? result["Image Width"].value
-    //     : 0
-    //   : 0;
-    // let imageLength = result
-    //   ? result["Image Height"]
-    //     ? result["Image Height"].value
-    //     : 0
-    //   : 0;
-    // let imageDescription = "";
-    
-    // if (result) {
-    //   if (
-    //     result.ImageDescription &&
-    //     result.ImageDescription.description
-    //   ) {
-    //     imageDescription =
-    //       result.ImageDescription.description.replace(
-    //         /[\n\/\\><-]+|\s+/g,
-    //         " "
-    //       );
-    //   }
-    
-    //   if (result.title && result.title.description) {
-    //     imgtitle = result.title.description.replace(
-    //       /[\n\/\\><-]+|\s+/g,
-    //       " "
-    //     );
-    //   }
-    //   if (result.subject && result.title.description) {
-    //     imgtags = result.subject.description.replace(
-    //       /[\n\/\\><-]+|\s+/g,
-    //       " "
-    //     );
-    //   }
-    // }
-    
-    // const data = new Data({
-    //   id: id,
-    //   title: imgtitle,
-    //   filename: fileName,
-    //   filetype: "image",
-    //   filesize: filesize,
-    //   url: url,
-    //   filedetails: imageDescription,
-    //   length: imageLength,
-    //   width: imageWidth,
-    //   imgtags: imgtags,
-    //   baseurl: baseurl,
-    // });
-    
-    // try {
-    //   // console.log(data.filedetails)
-    //   await data.save();
-    //   scandataval.nofiles = scandataval.nofiles + 1;
-    //   doccount++;
-    //   logger.info(`${filePath} scanned and saved to database`);
-    //   logger.debug(`Number of Document scanned are ${doccount}`);
-    // } catch (e) {
-    //   // console.log(e);
-    //   logger.error(
-    //     `Failed to save data to database ${filePath}. Skipping file. Scanning will continue`
-    //   );
-    //   const jsonError = JSON.stringify(e);
-    //   logger.debug(`Error:- ${jsonError}`);
-    // }
+const aspectRatioSize = maintainAspectRatio(360);
+for(let i = 1; i <= 8;i++){
+  const inputVideoPath = path.resolve(`sample${i}.mp4`);
+  const outputThumbnailPath = path.resolve(`thumbnail${i}.png`);
+  ffmpeg(inputVideoPath)
+    // .output(outputThumbnailPath)
+    .outputOptions('-vf', `thumbnail,scale=${aspectRatioSize}`, '-frames:v',`1`,'thumbnail.jpg')
+  //  .output(outputThumbnailPath)
+    .on('end', () => {
+      console.log('Thumbnail extracted successfully');
+    })
+    .on('error', (err) => {
+      console.error('Error extracting thumbnail:', err.message);
+    })
+    .save(outputThumbnailPath);
 }
 
-
-SomeFunc();
-//vendorFunc();
